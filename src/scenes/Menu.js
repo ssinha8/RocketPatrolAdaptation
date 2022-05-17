8e042c0ef73f084aa8237989979cc2e75e90ce82
Shloak Sinha <ssinha8@ucsc.edu>
2022-05-16 23:51:58 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -52,7 +52,7 @@ class Menu extends Phaser.Scene {
           // Novice mode
           game.settings = {
             balloonSpeed: 3,
-            gameTimer: 10000    
+            gameTimer: 60000    
           }
           this.sound.play('sfx_select');
           this.scene.start("playScene");    
